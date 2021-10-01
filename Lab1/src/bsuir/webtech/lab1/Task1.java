823c0e4f7b53a437fe85f862df7172ad14286c89
Киселёв Андрей <vkusnaya.travka@gmail.com>
2021-10-01 20:24:37 +0300

    task 1 done

---
+++ Lab1/src/bsuir/webtech/lab1/Task1.java
@@ -0,0 +1,23 @@
+package bsuir.webtech.lab1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("X: ");
+        double x = scan.nextDouble();
+
+        System.out.print("Y: ");
+        double y = scan.nextDouble();
+
+        System.out.printf("Result: %f", solve(x, y));
+    }
+
+    private static double solve(double x, double y) {
+        return (1 + Math.pow(Math.sin(x + y), 2))
+                / (2 + Math.abs(x - (2*x) / (1 + x * x * y * y)))
+                + x;
+    }
+}
