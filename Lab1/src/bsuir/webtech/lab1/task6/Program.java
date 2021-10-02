f17f538eae0d32d8451dfef7d74b0037beb0b32c
Киселёв Андрей <vkusnaya.travka@gmail.com>
2021-10-02 19:16:47 +0300

    task 6 done

---
+++ Lab1/src/bsuir/webtech/lab1/task6/Program.java
@@ -0,0 +1,45 @@
+package bsuir.webtech.lab1.task6;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("N: ");
+        int n = scan.nextInt();
+
+        int[] mas = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.printf("A[%d]: ", i);
+            mas[i] = scan.nextInt();
+        }
+
+        printMatrix(getMatrix(mas));
+    }
+
+    public static int[][] getMatrix(int[] mas) {
+        int n = mas.length;
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, k = i; k < n; j++, k++) {
+                mat[i][j] = mas[k];
+            }
+            for (int j = n - i, k = 0; j < n; j++, k++) {
+                mat[i][j] = mas[k];
+            }
+        }
+
+        return mat;
+    }
+
+    public static void printMatrix(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.printf("%d ", mat[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
