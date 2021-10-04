68694a6456d955bfebaaaa0304e2d7d5a6f606d8
Киселёв Андрей <vkusnaya.travka@gmail.com>
2021-10-04 21:06:23 +0300

    task 7 splitted

--- Lab1/src/bsuir/webtech/lab1/task7/Program.java
+++ Lab1/src/bsuir/webtech/lab1/task7/Program.java
@@ -1,6 +1,5 @@
 package bsuir.webtech.lab1.task7;
 
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Program {
@@ -16,7 +15,7 @@ public class Program {
             mas[i] = scan.nextDouble();
         }
 
-        shellSort(mas, Double::compare);
+        SortUtils.shellSort(mas, Double::compare);
 
         for (int i = 0; i < mas.length; i++) {
             System.out.printf("%f ", mas[i]);
@@ -24,15 +23,5 @@ public class Program {
         System.out.println();
     }
 
-    private static <T> void shellSort(T[] mas, Comparator<T> comparator) {
-        for (int step = mas.length / 2; step > 0; step /= 2) {
-            for (int i = step; i < mas.length; i++) {
-                for (int j = i - step; j >= 0 && comparator.compare(mas[j], mas[j + step]) > 0; j -= step) {
-                    T x = mas[j];
-                    mas[j] = mas[j + step];
-                    mas[j + step] = x;
-                }
-            }
-        }
-    }
+
 }
